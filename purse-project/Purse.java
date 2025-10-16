dced504ed171a9310ec031a8165aa2f4ce41bdc4
luca <luca.vicinanza04@gmail.com>
2025-10-16 23:32:04 +0200

    aggiustamenti emanuele

--- purse-project/Purse.java
+++ purse-project/Purse.java
@@ -13,27 +13,11 @@ public class Purse{
 		this.arr.add(aCoin);
 	}	
 
-	public void removeCoin(Coin aCoin){
-		boolean trovato = false;
-		for  (Coin moneta : arr){
-			if (moneta.getValue() == aCoin.getValue()){
-				arr.remove(moneta);
-				trovato = true;
-				System.out.println("moneta eliminata correttamente");
-			}
-		if(trovato == false){
-			System.out.println("moneta non trovata");
-		}
-
-		}
-	}
+	public boolean removeCoin(Coin aCoin){
+		return arr.remove(aCoin);
+	}	
 
 	public int find(Coin aCoin){
-		for(Coin moneta : arr){
-			if(moneta.equals(aCoin)){
-				return arr.indexOf(moneta);
-			}
-		}
-		return 0;
+		return  arr.indexOf(aCoin);
 	}
 }
